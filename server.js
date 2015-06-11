b6ecc734bcdd728df49e3c3610ddc4f354112da5
wacky6.AriesMBD <416707889@qq.com>
2015-06-11 15:11:38 +0800

    refine about's layout

--- server.js
+++ server.js
@@ -56,6 +56,6 @@ app.use(function *(next){
     }
 });
 
-app.use(serve(join(__dirname, "www"), {maxage: conf.cache?24*60*60*1000:0}));
+app.use(serve(join(__dirname, "www"), {maxage: conf.cache?10*60*1000:0}));
 
 app.listen(conf.port);
