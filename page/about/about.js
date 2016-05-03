ac33ff1cb2d277cad3d6ea5bac73025de002fcbe
wacky6.AriesMBP <416707889@qq.com>
2016-05-03 16:47:11 +0800

    add donation section

--- page/about/about.js
+++ page/about/about.js
@@ -5,16 +5,31 @@ const OVERLAY = {
         selector: '#alipay-qr-code',
         href:     'https://qr.alipay.com/aex04039win5g8swmisrb94',
         html: `
-            <img class="qr-code" width="344" height="344" src='alipay-qr-code.png' alt="std-weak's Alipay QR Code"></img>
-            <div class="title">客官赏点零花钱呗</div>
+            <img class="qr-code" src='alipay-qr-code.png' alt="std-weak's Alipay QR Code"></img>
+            <div class="hint">请用支付宝客户端扫描二维码<br>
+                              也可以用支付宝网页版转账喵<br>
+                              416707889@qq.com
+            </div>
+        `
+    },
+    'wechat-payment': {
+        selector: '#wechat-payment-qr-code',
+        // wx.tenpay does not launch wechat app
+        // href:     'https://wx.tenpay.com/f2f?t=AQAAAIUdo2fsBPH3kDVIzO12BjM%3D',
+        html:     `
+            <img class="qr-code" src='wechat-payment-qr-code.png' alt="std-weak's we-char payment QR Code"></img>
+            <div class="hint">长按二维码，保存到手机相册<br>
+                              然后用微信客户端扫描二维码
+            </div>
         `
     },
     'wechat': {
         selector: '#wechat-qr-code',
         href:     'https://weixin.qq.com/r/ZEuYgCjElnXprWzj9x52',
         html: `
-            <img src='wechat-qr-code.png' alt="std-weak's we-chat QR Code"></img>
+            <img class="qr-code" src='wechat-qr-code.png' alt="std-weak's we-chat QR Code"></img>
             <div class="title">别忘了告诉我你是谁</div>
+            <div class="hint">请用微信客户端扫描二维码</div>
         `
     }
 }
