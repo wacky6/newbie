fae14edc754b79b6c00b75074f5465c1b69811d5
Jiewei Qian <qjw@wacky.one>
2018-07-15 02:50:35 +0800

    remove legacy files

--- page/analytics.js
+++ page/analytics.js
@@ -1,12 +1,13 @@
 'use strict'
 window.addEventListener('load', function() {
     var noAnalytics = window.localStorage && window.localStorage.getItem('no-analytics')
+    var isLocalhost = /localhost|127\.0\.0\.1|::1/i.test(window.location.hostname)
     var doNotTrack  =    navigator.doNotTrack === 'yes'
                       || navigator.doNotTrack === '1'
                       || navigator.msDoNotTrack === '1'
                       || window.doNotTrack === '1'
 
-    if ( !doNotTrack && !noAnalytics ) {
+    if ( !doNotTrack && !noAnalytics && !isLocalhost ) {
         // track & analytics
         window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date
         ga('create', 'UA-72435418-1', 'auto')
