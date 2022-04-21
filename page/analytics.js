03779f509089e552fa287d9cd4ce5f5ac003f01f
Jiewei Qian <qjw@wacky.one>
2022-04-21 17:05:26 +1000

    update to Google GA4
    update tracking code to use Google Analytics 4 to prepare for universal analytics deprecation

--- page/analytics.js
+++ page/analytics.js
@@ -1,21 +1,28 @@
 'use strict'
 window.addEventListener('load', function() {
-    var noAnalytics = window.localStorage && window.localStorage.getItem('no-analytics')
-    var isLocalhost = /localhost|127\.0\.0\.1|::1/i.test(window.location.hostname)
-    var doNotTrack  =    navigator.doNotTrack === 'yes'
+    let noAnalytics = window.localStorage && window.localStorage.getItem('no-analytics')
+    let isLocalhost = /localhost|127\.0\.0\.1|::1/i.test(window.location.hostname)
+    let doNotTrack  =    navigator.doNotTrack === 'yes'
                       || navigator.doNotTrack === '1'
                       || navigator.msDoNotTrack === '1'
                       || window.doNotTrack === '1'
 
     if ( !doNotTrack && !noAnalytics && !isLocalhost ) {
-        // track & analytics
+        // Google UA.
         window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date
         ga('create', 'UA-72435418-1', 'auto')
         ga('send', 'pageview')
-
-        // insert analytics code
-        var s = document.createElement('script')
+        let s = document.createElement('script')
         s.setAttribute('src', '//www.google-analytics.com/analytics.js')
         document.body.appendChild(s)
+        
+        // Google GA4
+        s =  document.createElement('script')
+        s.setAttribute('src', '//www.googletagmanager.com/gtag/js?id=G-E2RBFS2JSW')
+        s.setAttribute('async', 'async')
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-E2RBFS2JSW');
     }
 })
